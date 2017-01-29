3cfb9a73eb8c02829ff8da9ebbd5d78917de37b7
佐々木厚吉 <oyakata@kshatriya.local>
2017-01-30 00:42:36 +0900

    modified script for centos image building.

--- image-build/idp-proxy/build-idp-proxy.sh
+++ image-build/idp-proxy/build-idp-proxy.sh
@@ -2,7 +2,7 @@
 
 BASE_DIR=$(cd $(dirname $0);cd ../../;pwd)
 
-#$BASE_DIR/bin/go-docker build --no-cache=true -t idp-proxy:1.0 ./
+$BASE_DIR/bin/go-docker build --no-cache=true -t idp-proxy:1.0 ./
 
-$BASE_DIR/bin/go-docker build -t idp-proxy:1.0 ./
+#$BASE_DIR/bin/go-docker build -t idp-proxy:1.0 ./
 
