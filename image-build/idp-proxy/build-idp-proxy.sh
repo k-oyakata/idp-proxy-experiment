ee46fb59aa3f3d811a9187bae999cfb553d0e359
佐々木厚吉 <oyakata@kshatriya.local>
2017-02-06 01:04:39 +0900

    Modified the build scripts for idp-proxy.

--- image-build/idp-proxy/build-idp-proxy.sh
+++ image-build/idp-proxy/build-idp-proxy.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+OPT=$1
+
 BASE_DIR=$(cd $(dirname $0);cd ../../;pwd)
 
+mkdir -p $BASE_DIR/image-build/idp-proxy/resources/tmp
+(cd $BASE_DIR/image-build/idp-proxy/resources/tmp; curl -O http://rpms.famillecollet.com/enterprise/remi-release-7.rpm)
 mkdir -p $BASE_DIR/image-build/idp-proxy/resources/keys
-cp -p ~/cert/nbhub.ecloud.nii.ac.jp.chained.cer $BASE_DIR/image-build/idp-proxy/resources/keys/idp-proxy.chained.cer
-cp -p ~/cert/nbhub.ecloud.nii.ac.jp.cer $BASE_DIR/image-build/idp-proxy/resources/keys/idp-proxy.cer
-cp -p ~/cert/nbhub.ecloud.nii.ac.jp.key $BASE_DIR/image-build/idp-proxy/resources/keys/idp-proxy.key
+cp -p ~/cert/idp-proxy/nbhub.ecloud.nii.ac.jp.chained.cer $BASE_DIR/image-build/idp-proxy/resources/keys/idp-proxy.chained.cer
+cp -p ~/cert/idp-proxy/nbhub.ecloud.nii.ac.jp.cer $BASE_DIR/image-build/idp-proxy/resources/keys/idp-proxy.cer
+cp -p ~/cert/idp-proxy/nbhub.ecloud.nii.ac.jp.key $BASE_DIR/image-build/idp-proxy/resources/keys/idp-proxy.key
 
-$BASE_DIR/bin/docker build --no-cache=true -t idp-proxy:latest ./
-
-#$BASE_DIR/bin/docker build -t idp-proxy:latest ./
+#$BASE_DIR/bin/docker build --no-cache=true -t idp-proxy:latest ./
+$BASE_DIR/bin/docker build $OPT --rm -t idp-proxy:latest ./
 
