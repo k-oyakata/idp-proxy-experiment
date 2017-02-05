fd9103c07c1934060b42368c85dfdbe56c657b73
佐々木厚吉 <oyakata@kshatriya.local>
2017-02-06 01:07:04 +0900

    Modified the build scripts for saml-idp.

--- image-build/saml-idp/build-saml-idp.sh
+++ image-build/saml-idp/build-saml-idp.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+OPT=$1
+
 BASE_DIR=$(cd $(dirname $0);cd ../../;pwd)
 
-#$BASE_DIR/bin/go-docker build --no-cache=true -t saml-idp:1.0 ./
+mkdir -p $BASE_DIR/image-build/saml-idp/resources/tmp
+(cd $BASE_DIR/image-build/saml-idp/resources/tmp; curl -O http://rpms.famillecollet.com/enterprise/remi-release-7.rpm)
+mkdir -p $BASE_DIR/image-build/saml-idp/resources/keys
+cp -p ~/cert/saml-idp/saml-idp.chained.cer $BASE_DIR/image-build/saml-idp/resources/keys/
+cp -p ~/cert/saml-idp/saml-idp.cer $BASE_DIR/image-build/saml-idp/resources/keys/
+cp -p ~/cert/saml-idp/saml-idp.key $BASE_DIR/image-build/saml-idp/resources/keys/
 
-$BASE_DIR/bin/go-docker build -t saml-idp:1.0 ./
+#$BASE_DIR/bin/docker build --no-cache=true -t saml-idp:1.0 ./
+$BASE_DIR/bin/docker build $OPT --rm -t saml-idp:latest ./
 
