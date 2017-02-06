0e5bf029faff8584d71115498fae94a705da1afa
佐々木厚吉 <oyakata@kshatriya.local>
2017-02-07 04:30:53 +0900

    Update for development.

--- image-build/saml-idp/resources/simplesamlphp/metadata/saml20-sp-remote.php
+++ image-build/saml-idp/resources/simplesamlphp/metadata/saml20-sp-remote.php
@@ -1,12 +1,6 @@
 <?php
-/**
- * SAML 2.0 remote SP metadata for SimpleSAMLphp.
- *
- * See: https://simplesamlphp.org/docs/stable/simplesamlphp-reference-sp-remote
- */
-
-$metadata['https://web.demo.org/simplesaml/module.php'] = array (
-  'entityid' => 'https://web.demo.org/simplesaml/module.php',
+$metadata['https://auth-proxy:441/simplesaml/module.php'] = array (
+  'entityid' => 'https://auth-proxy:441/simplesaml/module.php',
   'contacts' => 
   array (
   ),
@@ -16,25 +10,25 @@ $metadata['https://web.demo.org/simplesaml/module.php'] = array (
     0 => 
     array (
       'Binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST',
-      'Location' => 'https://web.demo.org/simplesaml/module.php/saml/sp/saml2-acs.php/default-sp',
+      'Location' => 'https://auth-proxy:441/simplesaml/module.php/saml/sp/saml2-acs.php/default-sp',
       'index' => 0,
     ),
     1 => 
     array (
       'Binding' => 'urn:oasis:names:tc:SAML:1.0:profiles:browser-post',
-      'Location' => 'https://web.demo.org/simplesaml/module.php/saml/sp/saml1-acs.php/default-sp',
+      'Location' => 'https://auth-proxy:441/simplesaml/module.php/saml/sp/saml1-acs.php/default-sp',
       'index' => 1,
     ),
     2 => 
     array (
       'Binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Artifact',
-      'Location' => 'https://web.demo.org/simplesaml/module.php/saml/sp/saml2-acs.php/default-sp',
+      'Location' => 'https://auth-proxy:441/simplesaml/module.php/saml/sp/saml2-acs.php/default-sp',
       'index' => 2,
     ),
     3 => 
     array (
       'Binding' => 'urn:oasis:names:tc:SAML:1.0:profiles:artifact-01',
-      'Location' => 'https://web.demo.org/simplesaml/module.php/saml/sp/saml1-acs.php/default-sp/artifact',
+      'Location' => 'https://auth-proxy:441/simplesaml/module.php/saml/sp/saml1-acs.php/default-sp/artifact',
       'index' => 3,
     ),
   ),
@@ -43,7 +37,7 @@ $metadata['https://web.demo.org/simplesaml/module.php'] = array (
     0 => 
     array (
       'Binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect',
-      'Location' => 'https://web.demo.org/simplesaml/module.php/saml/sp/saml2-logout.php/default-sp',
+      'Location' => 'https://auth-proxy:441/simplesaml/module.php/saml/sp/saml2-logout.php/default-sp',
     ),
   ),
   'keys' => 
@@ -53,14 +47,15 @@ $metadata['https://web.demo.org/simplesaml/module.php'] = array (
       'encryption' => false,
       'signing' => true,
       'type' => 'X509Certificate',
-      'X509Certificate' => 'MIIDaTCCAlGgAwIBAgIJAKpdxELULXnhMA0GCSqGSIb3DQEBCwUAMEsxCzAJBgNVBAYTAkpBMQ4wDAYDVQQIDAVKYXBhbjEOMAwGA1UEBwwFVG9reW8xHDAaBgNVBAoME0RlZmF1bHQgQ29tcGFueSBMdGQwHhcNMTYxMTAzMDUzMDQ3WhcNMjYxMTAzMDUzMDQ3WjBLMQswCQYDVQQGEwJKQTEOMAwGA1UECAwFSmFwYW4xDjAMBgNVBAcMBVRva3lvMRwwGgYDVQQKDBNEZWZhdWx0IENvbXBhbnkgTHRkMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAw/h2T0JP+T4GUoTr59AtUC0rfMzzOY95+aF2U58PLol6t//fQNTI486g1I/UpNtTwLdPSE9yzGNyYwaotfINe2EqNrQROIVQlaqALJ3Q2+AOj4cvNhU3aYlQzb0j1Lq3DCp21LS1boUS4My9IELkPf+AUXpr1XTRSONEuViHvu8N/4RvI6n4dFSWSRFcc+S4nTUKUjAvn3N6a6LCV6bZkXTZrk/AsrC44utj3w+YNc2Yyuj2kWBeeOEJu/H+jWyz6wpE/lvyv7WfUVyyJtVh+k2HIrbShlbhRAqb4V2OBGxynA1UN2YUfLaiuOUYBiolnX5yP6otFGcyTq/4mmExkwIDAQABo1AwTjAdBgNVHQ4EFgQUZruLvZfEz1j4vAMUtOWBWu4lsjswHwYDVR0jBBgwFoAUZruLvZfEz1j4vAMUtOWBWu4lsjswDAYDVR0TBAUwAwEB/zANBgkqhkiG9w0BAQsFAAOCAQEAOrc9rlrJjaj4GhbrrRJfYMw+6ZiEZ7+PEgKoYcjVuGXfDSYWzOEACT/yvictaJ+CWZzGvwG5EhP4hVoYndjnwjetXy3T0wds2X3yxZ0qUsrTG9dYRwH81Lhuh+3W4raLix6/CnhzQA/xjOAaClCmfnsqnXOB3LX/+xKaSjOd5bWJzAHZtMnrlCDXbomeaL4+AW7xhzFFaM6is//449au7+nznKEQW84yr5DioPeriVaUXPgnLtH+8TNnlna1I/5+QegJyiO0/RjnCZvr09HUgeCtY65LIl8m8RcgBvX3y2l99gjPYqAXROjmLek6db6zbZOV5LIcTc1iH1JJOWvAxQ==',
+      'X509Certificate' => 'MIID5jCCAs6gAwIBAgIJAJfAmdl/3XbEMA0GCSqGSIb3DQEBBQUAMFUxCzAJBgNVBAYTAkpQMQ4wDAYDVQQIEwVUb2t5bzETMBEGA1UEBxMKQ2hpeW9kYS1rdTEMMAoGA1UEChMDTklJMRMwEQYDVQQDEwphdXRoLXByb3h5MB4XDTE3MDIwMTE3MDQ1MloXDTE3MDMwMzE3MDQ1MlowVTELMAkGA1UEBhMCSlAxDjAMBgNVBAgTBVRva3lvMRMwEQYDVQQHEwpDaGl5b2RhLWt1MQwwCgYDVQQKEwNOSUkxEzARBgNVBAMTCmF1dGgtcHJveHkwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQDblf1cBxLIX/qo4a7kMS3HAy9ZZFgCjxvAVJUln2w84r+QwM1qJUkw8p8CB7nQFIuhV8/v0GBUlJL7c+v1mOOWe1b4yvvIQdNpH3VCh6FlLkXDoIEiD9YmqrYorLyC/bhZsfOYcX7iDlrXLWNRUw6MjXLpmR66+Dkbo3YoGb+sZ87coGBXM0pzlUBKV+xZETcJOxzv2uE2ZcnmcK0G6MNakcR36vaFlSbSQRdo5f5VLjSSFRGkbIYE+H6TsWcHU71WXez56HcHSqia3eoyHTMkIbjJXwaXdRZB0SHTjZ/0uLn6CzmlHm2JFNV71iEsC4aq2LFWV5+npfGq0fD52QbnAgMBAAGjgbgwgbUwHQYDVR0OBBYEFJXKy2XwW+JX2UCkqcHINvwXgJxYMIGFBgNVHSMEfjB8gBSVystl8FviV9lApKnByDb8F4CcWKFZpFcwVTELMAkGA1UEBhMCSlAxDjAMBgNVBAgTBVRva3lvMRMwEQYDVQQHEwpDaGl5b2RhLWt1MQwwCgYDVQQKEwNOSUkxEzARBgNVBAMTCmF1dGgtcHJveHmCCQCXwJnZf912xDAMBgNVHRMEBTADAQH/MA0GCSqGSIb3DQEBBQUAA4IBAQAABoBsLzTAX9hguM7sCMDFVN/vitQfZwz53g/41vDxZDkVm/GqT3jLMMVBYK0R+dkhCSMvqw3fm1HMc3BqIfCm2YLwRM1dPfv22595BA2lWaQMmwgSHBOhuoDb7yuVp0OmQ5h3qPq67dSQ4LIlU6ZXxP3DUQkhr3zfK3djnHRYKkQYIdrytL8SFZkdW4XuSAAWm72MbAdkYCyLAj4JZYozPRd0s5t3Yd4gfY/YqW0c9GKDhoIVRtuDMfQ0/6rQ8LxX4xZWZvubBGiuiJXgttNwyGjFgBFoPrVnrkzTXYHrHpnC3ETX4q8LujYH/CI7U0dbLvjOQweaFezHKWk4VTu+',
     ),
     1 => 
     array (
       'encryption' => true,
       'signing' => false,
       'type' => 'X509Certificate',
-      'X509Certificate' => 'MIIDaTCCAlGgAwIBAgIJAKpdxELULXnhMA0GCSqGSIb3DQEBCwUAMEsxCzAJBgNVBAYTAkpBMQ4wDAYDVQQIDAVKYXBhbjEOMAwGA1UEBwwFVG9reW8xHDAaBgNVBAoME0RlZmF1bHQgQ29tcGFueSBMdGQwHhcNMTYxMTAzMDUzMDQ3WhcNMjYxMTAzMDUzMDQ3WjBLMQswCQYDVQQGEwJKQTEOMAwGA1UECAwFSmFwYW4xDjAMBgNVBAcMBVRva3lvMRwwGgYDVQQKDBNEZWZhdWx0IENvbXBhbnkgTHRkMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAw/h2T0JP+T4GUoTr59AtUC0rfMzzOY95+aF2U58PLol6t//fQNTI486g1I/UpNtTwLdPSE9yzGNyYwaotfINe2EqNrQROIVQlaqALJ3Q2+AOj4cvNhU3aYlQzb0j1Lq3DCp21LS1boUS4My9IELkPf+AUXpr1XTRSONEuViHvu8N/4RvI6n4dFSWSRFcc+S4nTUKUjAvn3N6a6LCV6bZkXTZrk/AsrC44utj3w+YNc2Yyuj2kWBeeOEJu/H+jWyz6wpE/lvyv7WfUVyyJtVh+k2HIrbShlbhRAqb4V2OBGxynA1UN2YUfLaiuOUYBiolnX5yP6otFGcyTq/4mmExkwIDAQABo1AwTjAdBgNVHQ4EFgQUZruLvZfEz1j4vAMUtOWBWu4lsjswHwYDVR0jBBgwFoAUZruLvZfEz1j4vAMUtOWBWu4lsjswDAYDVR0TBAUwAwEB/zANBgkqhkiG9w0BAQsFAAOCAQEAOrc9rlrJjaj4GhbrrRJfYMw+6ZiEZ7+PEgKoYcjVuGXfDSYWzOEACT/yvictaJ+CWZzGvwG5EhP4hVoYndjnwjetXy3T0wds2X3yxZ0qUsrTG9dYRwH81Lhuh+3W4raLix6/CnhzQA/xjOAaClCmfnsqnXOB3LX/+xKaSjOd5bWJzAHZtMnrlCDXbomeaL4+AW7xhzFFaM6is//449au7+nznKEQW84yr5DioPeriVaUXPgnLtH+8TNnlna1I/5+QegJyiO0/RjnCZvr09HUgeCtY65LIl8m8RcgBvX3y2l99gjPYqAXROjmLek6db6zbZOV5LIcTc1iH1JJOWvAxQ==',
+      'X509Certificate' => 'MIID5jCCAs6gAwIBAgIJAJfAmdl/3XbEMA0GCSqGSIb3DQEBBQUAMFUxCzAJBgNVBAYTAkpQMQ4wDAYDVQQIEwVUb2t5bzETMBEGA1UEBxMKQ2hpeW9kYS1rdTEMMAoGA1UEChMDTklJMRMwEQYDVQQDEwphdXRoLXByb3h5MB4XDTE3MDIwMTE3MDQ1MloXDTE3MDMwMzE3MDQ1MlowVTELMAkGA1UEBhMCSlAxDjAMBgNVBAgTBVRva3lvMRMwEQYDVQQHEwpDaGl5b2RhLWt1MQwwCgYDVQQKEwNOSUkxEzARBgNVBAMTCmF1dGgtcHJveHkwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQDblf1cBxLIX/qo4a7kMS3HAy9ZZFgCjxvAVJUln2w84r+QwM1qJUkw8p8CB7nQFIuhV8/v0GBUlJL7c+v1mOOWe1b4yvvIQdNpH3VCh6FlLkXDoIEiD9YmqrYorLyC/bhZsfOYcX7iDlrXLWNRUw6MjXLpmR66+Dkbo3YoGb+sZ87coGBXM0pzlUBKV+xZETcJOxzv2uE2ZcnmcK0G6MNakcR36vaFlSbSQRdo5f5VLjSSFRGkbIYE+H6TsWcHU71WXez56HcHSqia3eoyHTMkIbjJXwaXdRZB0SHTjZ/0uLn6CzmlHm2JFNV71iEsC4aq2LFWV5+npfGq0fD52QbnAgMBAAGjgbgwgbUwHQYDVR0OBBYEFJXKy2XwW+JX2UCkqcHINvwXgJxYMIGFBgNVHSMEfjB8gBSVystl8FviV9lApKnByDb8F4CcWKFZpFcwVTELMAkGA1UEBhMCSlAxDjAMBgNVBAgTBVRva3lvMRMwEQYDVQQHEwpDaGl5b2RhLWt1MQwwCgYDVQQKEwNOSUkxEzARBgNVBAMTCmF1dGgtcHJveHmCCQCXwJnZf912xDAMBgNVHRMEBTADAQH/MA0GCSqGSIb3DQEBBQUAA4IBAQAABoBsLzTAX9hguM7sCMDFVN/vitQfZwz53g/41vDxZDkVm/GqT3jLMMVBYK0R+dkhCSMvqw3fm1HMc3BqIfCm2YLwRM1dPfv22595BA2lWaQMmwgSHBOhuoDb7yuVp0OmQ5h3qPq67dSQ4LIlU6ZXxP3DUQkhr3zfK3djnHRYKkQYIdrytL8SFZkdW4XuSAAWm72MbAdkYCyLAj4JZYozPRd0s5t3Yd4gfY/YqW0c9GKDhoIVRtuDMfQ0/6rQ8LxX4xZWZvubBGiuiJXgttNwyGjFgBFoPrVnrkzTXYHrHpnC3ETX4q8LujYH/CI7U0dbLvjOQweaFezHKWk4VTu+',
     ),
   ),
 );
+
